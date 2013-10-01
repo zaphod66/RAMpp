dfffc7d8b8c7982aed82ac83187b35ebc45fa776
Norbert Scheller <nscheller@actian.com>
2013-10-01 11:51:53 +0200

    Parse IF command

--- src/ParserApp.scala
+++ src/ParserApp.scala
@@ -1,3 +1,5 @@
+import java.io.FileReader
+
 object ParserApp extends App {
 
   println("ParserApp")
--- src/util/Parser.scala
+++ src/util/Parser.scala
@@ -11,3 +11,14 @@ case class WRITEcmd(outvar: String) extends Command
 case class READcmd(invar: String) extends Command
 case class ASSIGNMENTcmd(ass_var: String, action: String) extends Command
 
+object RAMparser extends JavaTokenParsers {
+  def commands = rep(command) ^^ { case cmds => Commands(cmds) }
+  
+  def command  = ifCmd | failure("unexpected symbol")
+  
+  def ifCmd: Parser[Command] = ("if" ~ ident ~ "=" ~ "0" ~ "then" ~ commands ~ optElse ~ "end") ^^ 
+                                     { case "if" ~ id ~ "=" ~ "0" ~ "then" ~ thenpart ~ elsepart ~ "end" => 
+                                       IFcmd(id,thenpart,elsepart) }
+   def optElse: Parser[Commands] = opt("else"~commands) ^^ { case None => Commands(Nil)
+                                                             case Some("else"~cmds) => cmds } 
+}
