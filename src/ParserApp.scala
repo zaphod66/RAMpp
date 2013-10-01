7495092ae392fd58cea6a353c88271ea0960a6d7
Norbert Scheller <nscheller@actian.com>
2013-10-01 13:51:09 +0200

    App skeleton for parse -> eval

--- src/ParserApp.scala
+++ src/ParserApp.scala
@@ -1,7 +1,17 @@
 import java.io.FileReader
 
+import util._   // Parser, Evaluator
+
 object ParserApp extends App {
 
   println("ParserApp")
   
+  val reader = new FileReader("RAM.src")
+  val result = RAMparser.parseAll(RAMparser.commands, reader)
+  if (result.successful) {
+    val parseTree = result.get
+    Evaluator.eval(parseTree.cmds)
+  } else {
+    println(result)
+  }
 }
